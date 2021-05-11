081189b928f53403540715246f91c32952bff9a3
Nikolaev Vadim <nikolaev1024@gmail.com>
2021-05-11 11:55:40 +0300

    Add header guard

--- ARMDText/EventProgramName.h
+++ ARMDText/EventProgramName.h
@@ -1,1 +1,9 @@
-#pragma once
+#ifndef EVENT_PROGRAM_NAME
+#define EVENT_PROGRAM_NAME
+#include "ARMDEventParser.h"
+
+void EventProgramNameFree(ProgName* prog_name);
+int EventProgramName(ProgName** prog_name, ARMDParserData* armd_parser_data);
+
+#endif // !EVENT_PROGRAM_NAME
+
